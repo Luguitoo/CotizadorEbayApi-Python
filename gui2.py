14942161c0c9fc6b7872cdc6aaf4d12de4ed13ad
Carlos Lugo <lugostein201@gmail.com>
2023-07-28 16:57:44 -0300

    Alpha 0.03

--- gui2.py
+++ gui2.py
@@ -1,19 +1,18 @@
-
+#imports
+import PySimpleGUI as sg
+#Web scraping para cotizacion del dolar del dia
+import requests
+from bs4 import BeautifulSoup
 #EbaySDK
 from ebaysdk.finding import Connection
 import datetime
 from deep_translator import GoogleTranslator
 
-#Web scraping para cotizacion del dolar del dia
-import requests
-from bs4 import BeautifulSoup
 r = requests.get('https://www.cambioschaco.com.py') #Url de donde obtener los datos
 soup = BeautifulSoup(r.text, 'html.parser')
 cotizacion = soup.find_all('span', class_="sale", limit = 1) #Dolar
 #print(cotizacion[0].text.replace('.', ""))
 dolar = float(cotizacion[0].text.replace('.', ""))
-print(f"Dolar hoy: {cotizacion[0].text}")
-
 
 API_KEY = ""
 
@@ -22,14 +21,6 @@ with open('key.txt', 'r') as f:
     API_KEY = f.readline().strip()
     #print(API_KEY)
 
-#Pillow para crea imagenes
-from PIL import Image #Para cargar las imagenes
-from PIL import ImageDraw #para dibujar
-from PIL import ImageOps #Reescalado
-from PIL import ImageFont #Fuentes
-dir_img = "./imagenes/" #carpeta de imagenes
-dir_rec = "./recursos/" #carpeta de recursos
-dir_save = "./post/" #carpeta de guardado de img
 class Ebay(object):
     def __init__(self, API_KEY, keyword, capacity, resultado):
         self.api_key = API_KEY
@@ -89,7 +80,8 @@ class Ebay(object):
             print(e)
             print(e.response.dict())
     def parse(self):
-        pass
+        pass  
+    
 #Convertir el precio a un numero con puntos y redondeado
 def pre_puntos(precio):
     a = list(precio)
@@ -104,87 +96,89 @@ def pre_puntos(precio):
         if cont % 3 == 0 and j != len(precio) - 1:
             nr = nr + "."
     #print(nr)
-    return nr
+    return nr  
+#tema simplegui
+sg.theme("reddit")
+
+#Cabecera, Productos
+headings= ['Producto', 'Capacidad','Costo', 'Venta', 'Condición']
+data = []
+def make_win1():
+    # Columna 1:
+    col1 = [[sg.Text("Cotización del dia:")],
+            [sg.Text(f"Dolar: {cotizacion[0].text} Gs")],
+            [sg.Text("Producto:"), sg.Input(key='pro', size=(20, 1))],
+            [sg.Button('Buscar'), sg.Button('Borrar'), sg.Button('Modificar'),],
+            ]
+     # Columna 2:
+
+    col2 = [[sg.Text(" ",key='bus_p'), sg.Text(" ",key='bus_c')],
+            [sg.Table(data, headings=headings, justification='left', key='ta_p', enable_events=True, col_widths=[60, 10, 15, 60])],
+            [sg.Text("Costo total:  "), sg.Text(" ",key='ttv'), sg.Text("Ganancias:  "), sg.Text(" ",key='ttg'), sg.Button('Exportar presupusto')]]
+
+    layout = [[sg.Column(col1), sg.Column(col2, element_justification='')]]
+
+    return sg.Window('Cotizaciones de productos', layout, location=(800,600), finalize=True)
 
 
-def conv_img(ruta, nombre, precios):
-    #Crea una imagen del producto con su precio...
-    print(f"Procesando: {nombre}")
+window1= make_win1(), None
 
-    #Background
-    image = Image.open(dir_img + f"{nombre}.png")
-    draw = ImageDraw.Draw(image)
+# Definimos una lista para almacenar los productos
+productos = []
+#Actualizacion tabla de productos
+def act():
+    window["ta_p"].update(values=productos)
+    window.refresh()
 
+while True:
+    window, event, values = sg.read_all_windows()
 
-    #Escribimos precio y titulo #500, 900
-
-    precio1 = precios[0] + " Gs"
-    precio2 = precios[1] + " Gs"
-    if len(precios) == 2:
-        fuente = ImageFont.truetype(dir_rec + "Antonio-Bold.ttf", size=67, encoding="UTF-8")
-        ancho, alto = draw.textsize(precio1, font=fuente)
-        draw.text((800 - ancho // 2, 318 - alto // 2), precio1, font=fuente,fill=(39, 39, 39))
-        draw.text((805 - ancho // 2, 684 - alto // 2), precio2, font=fuente,fill=(39, 39, 39))
-    else:
-        precio3 = precios[2] + " Gs"
-        fuente = ImageFont.truetype(dir_rec + "Antonio-Bold.ttf", size=60, encoding="UTF-8")
-        ancho, alto = draw.textsize(precio1, font=fuente)
-        draw.text((800 - ancho // 2, 267 - alto // 2), precio1, font=fuente,fill=(39, 39, 39))
-        draw.text((805 - ancho // 2, 538 - alto // 2), precio2, font=fuente,fill=(39, 39, 39))
-        draw.text((805 - ancho // 2, 775 - alto // 2), precio3, font=fuente,fill=(39, 39, 39))
-    #guardamos la imagen del producto
-    image.save(dir_save + nombre + ".png")
-    print("Success")
-    return 0
-
-if __name__ == "__main__":
-    # Buscador
-    celulares = ["Iphone X", "Iphone 11", "Iphone 12", "Iphone 13"]
-    capacidades = ["64","256","128"]
-    #11 tiene los 3, X 64 y 256, 12 los 3, 13 solo 128 y 256
-    for i in range(0, len(celulares)):
-        precios_imagen = []
+    if event == sg.WIN_CLOSED or event == 'Salir':
+        window.close()
+        ##if window == window2:       # if closing win 2, mark as closed
+            ##window2 = None
+        if window == window1: # if closing win 1, exit program
+            break    
+    elif event == 'Buscar' :
+        pro = (values['pro']).lower()   #minisculas
+        capacidades = []
+        if pro == "iphone x" or pro == "iphone 10":
+                capacidades = ["64", "256"]
+        if pro == "iphone 11":
+                capacidades = ["64","128","256"]
+        if pro == "iphone 12":
+                capacidades = ["64","128", "256"]
+        if pro == "iphone 13":
+                capacidades = ["128","256"]
+        ##print(capacidades)
         for x in range(0, len(capacidades)):
-            #Ya que algunos no poseen las mismas capacidades
-            if i == 0 and x == 2:
-                break
-            if i == 2 and x != 0:
-                if x == 2:
-                    break
-                x += 1
-            if i == 3:
-                if x == 2:
-                    break
-                if x == 0 or x==1:
-                    x += 1
-            print(f"Busqueda: {celulares[i]} de {capacidades[x]}")
-            #Conexion a la API y busqueda del celular
-            e = Ebay(API_KEY, celulares[i], capacidades[x], "")
-            e.fetch()
-            e.parse()
-            #Mejor resultado
-            busqueda = e.resultado
-            # print(busqueda)
-            # Tiene 7% de taxes  y 50mil cuesta el envio, recargar eso
-            precio = str(int(float(busqueda.sellingStatus.currentPrice.value) * dolar))
-            preciov = str((int((float(busqueda.sellingStatus.currentPrice.value) + 70) * dolar)))
-            # print(precio)
-            a = list(precio)
-            # print(precio)
-            #print(preciov)
-            preciob = pre_puntos(precio)
-            preciov = pre_puntos(preciov)
-            print("Mejor elección: ")
-            titulo = str(busqueda.title)
-            # print(titulo)
-            traductor = GoogleTranslator(source='en', target='es')
-            titulo = traductor.translate(busqueda.title)
-            condicion = traductor.translate(busqueda.condition.conditionDisplayName)
-            # print(titulo)
-            print(f"Titulo: {busqueda.title}, Precio de Ebay: {preciob}, Precio de venta: {preciov} Gs, Condicion: {condicion}")
-            precios_imagen.append(preciov)
-        print(precios_imagen)
-        dir_im = dir_img + celulares[i]
-        conv_img(dir_im, celulares[i], precios_imagen)
-
-
+                #Conexion a la API y busqueda del celular
+                e = Ebay(API_KEY, pro, capacidades[x], "")
+                e.fetch()
+                e.parse()
+                #Mejor resultado
+                busqueda = e.resultado
+                #print(busqueda)
+                # Tiene 7% de taxes  y 50mil cuesta el envio, recargar eso
+                precio = str(int(float(busqueda.sellingStatus.currentPrice.value) * dolar))
+                preciov = str((int((float(busqueda.sellingStatus.currentPrice.value) + 70) * dolar)))
+                # print(precio)
+                a = list(precio)
+                # print(precio)
+                #print(preciov)
+                preciob = pre_puntos(precio)
+                preciov = pre_puntos(preciov)
+                print("Mejor elección: ")
+                titulo = str(busqueda.title)
+                # print(titulo)
+                traductor = GoogleTranslator(source='en', target='es')
+                titulo = traductor.translate(busqueda.title)
+                condicion = traductor.translate(busqueda.condition.conditionDisplayName)
+                # print(titulo)
+                print(f"Titulo: {busqueda.title}, Precio de Ebay: {preciob}, Precio de venta: {preciov} Gs, Condicion: {condicion}")
+                # Agregamos eñ producto a la lista
+                nombre = busqueda.title.split()
+                producto = [nombre[1] + " " + nombre[2],capacidades[x] + " GB",preciob, preciov, condicion]
+                productos.append(producto)
+                act()
+                producto = []
